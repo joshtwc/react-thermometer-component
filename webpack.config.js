69a9822150668cccfe8acc8899f8b08df241f6f7
Josh Wood <josh@thewateringcan.ca>
2026-01-20 12:27:47 -0500

    Update dependencies & fix vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
 'use strict'
 
-let webpack = require('webpack')
+const path = require('path')
 
-let config = {
+module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    path: 'lib/',
-    filename: 'index.js'
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js',
+    library: {
+      type: 'commonjs2'
+    }
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['react']
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         }
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
 }
-
-module.exports = config
